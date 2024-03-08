008fe8b4c55a53b8ec9c446ae110b507370ada96
westonmeacham <weston.meacham@gmail.com>
2024-03-08 13:38:25 -0500

    simple changes nothing big

---
+++ js/simple-js-app/js/scripts.js
@@ -0,0 +1,13 @@
+let myName = 'Bob';
+document.write(myName);
+
+myName = 'John Doe';
+document.write(myName);
+
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
