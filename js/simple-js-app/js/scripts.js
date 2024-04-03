714cc64585531f291adc3d6b1c8f62311b609f14
westonmeacham <weston.meacham@gmail.com>
2024-04-03 12:11:44 -0600

    Update scripts.js

--- js/simple-js-app/js/scripts.js
+++ js/simple-js-app/js/scripts.js
@@ -1,13 +1,3 @@
-let myName = 'Bob';
-document.write(myName);
-
-myName = 'John Doe';
-document.write(myName);
-
-let simpleAddition = 2 + 2;
-document.write(simpleAddition);
-
-let size = 100;
-let doubleSize = size * 2;
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
+pokemonList=[{name: "pikachu", height: "3", type: ["lightning","speed"]},
+{name: "squirtle", height: "4", type: ["water","defense"]},
+{name: "Mewtwo", height: "12", type: ["psychic","legendary"]}]
