517a4fafbd6bc24cc9aae08394ab1ec94f2fae52
westonmeacham <weston.meacham@gmail.com>
2024-04-17 16:01:06 -0600

    Update scripts.js

--- js/simple-js-app/js/scripts.js
+++ js/simple-js-app/js/scripts.js
@@ -1,3 +1,45 @@
-pokemonList=[{name: "pikachu", height: "3", type: ["lightning","speed"]},
-{name: "squirtle", height: "4", type: ["water","defense"]},
-{name: "Mewtwo", height: "12", type: ["psychic","legendary"]}]
+pokemonList=[
+    {name: "pikachu", height: "3", type: ["lightning","speed"]},
+    {name: "squirtle", height: "4", type: ["water","defense"]},
+    {name: "Mewtwo", height: "12", type: ["psychic","legendary"]}
+];
+
+/* Just an example of an erray with a loop
+let personAge = [
+    {name: 'person1', age: 16},
+    {name: 'person2', age: 10},
+    {name: 'person3', age: 25}
+];
+
+for (let i=0; i < personAge.length; i++){
+    if (personAge[i].age <19 && personAge[i].age >13){
+        console.log(personAge[i].name + " is a teenager");
+    }else if (personAge[i].age <13){
+        console.log(personAge[i].name + " is a child");
+    }else {
+        console.log(personAge[i].name + " is an adult");
+    }
+} 
+*/ 
+
+/* More examples of loops
+let text = "";
+let i = 0;
+
+do {
+    text = text + " "+ i;
+    i++
+}
+while (i < 10);
+console.log(text);
+
+let fruits = ["apple", "banana", "grape"]
+let text = "";
+let i = 0;
+
+while (fruites[i]){
+    text = text + " " + fruits[i]:
+    i++;
+}
+console.log(text);
+*/
